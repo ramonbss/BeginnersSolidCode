c920593b79ec5d236338609f46d3d77f3b0f8257
Ramon Brito <ramon.mecatec@gmail.com>
2022-08-12 09:33:35 -0300

    Add hangman.py

---
+++ hangman/hangman.py
@@ -0,0 +1,19 @@
+"""
+        Hangman game but with SOLID principles applied (at least those I now by the time)
+"""
+
+
+class HangManGame():
+    'Hang Man game class'
+    print('aqui é a classe')
+
+    def __init__(self) -> None:
+        print('class init')
+
+
+def main():
+    hang_man = HangManGame()
+
+
+if __name__ == '__main__':
+    main()
