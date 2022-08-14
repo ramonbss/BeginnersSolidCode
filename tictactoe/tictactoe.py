cea7784b0fc4099b4479dd769fc0ebab4edeb7c2
Ramon Brito <ramon.mecatec@gmail.com>
2022-08-13 22:08:51 -0300

    Implement start_game ; Add player_name property

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -8,6 +8,9 @@ class TicTacToeGame:
     _GAME_COMPUTER_HUMAN = 1
     _GAME_COMPUTER_COMPUTER = 2
 
+    _P1_NAME = 'Player1'
+    _P2_NAME = 'Player2'
+
     def __init__(self) -> None:
         self._player1: IPlayer
         self._player2: IPlayer
@@ -16,9 +19,20 @@ class TicTacToeGame:
     def start_game(self) -> None:
         game_type = self._prompt_game_type()
         self._initialize_players(game_type)
-        current_player =
+        current_player = random.choice([self._player1, self._player2])
         while True:
-            pass
+            self._board.print_board()
+            print(f'{current_player.player_name} turn')
+            current_player.pick_board_position(
+                self._board.taken_positions
+            )
+            if self._board.check_if_winner():
+                print(f'Player {current_player.player_name} won the game!!!')
+                break
+            if current_player == self._player1:
+                current_player = self._player2
+            else:
+                current_player = self._player1
 
     def _prompt_game_type(self) -> int:
         while True:
@@ -32,18 +46,22 @@ class TicTacToeGame:
 
     def _initialize_players(self, game_type: int):
         if game_type == self._GAME_HUMAN_HUMAN:
-            self._player1 = HumanPlayer(self._board.p1_symbol)
-            self._player2 = HumanPlayer(self._board.p2_symbol)
+            self._player1 = HumanPlayer(self._board.p1_symbol, self._P1_NAME)
+            self._player2 = HumanPlayer(self._board.p2_symbol, self._P2_NAME)
         elif game_type == self._GAME_COMPUTER_HUMAN:
-            self._player1 = HumanPlayer(self._board.p1_symbol)
-            self._player2 = ComputerPlayer(self._board.p2_symbol)
+            self._player1 = HumanPlayer(self._board.p1_symbol, self._P1_NAME)
+            self._player2 = ComputerPlayer(
+                self._board.p2_symbol, self._P2_NAME)
         elif game_type == self._GAME_COMPUTER_COMPUTER:
-            self._player1 = ComputerPlayer(self._board.p1_symbol)
-            self._player2 = ComputerPlayer(self._board.p2_symbol)
+            self._player1 = ComputerPlayer(
+                self._board.p1_symbol, self._P1_NAME)
+            self._player2 = ComputerPlayer(
+                self._board.p2_symbol, self._P2_NAME)
 
 
 def main():
-    pass
+    tictactoe = TicTacToeGame()
+    tictactoe.start_game()
 
 
 if __name__ == '__main__':
